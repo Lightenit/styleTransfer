642c45dcaa8e8593d9d8994066f613fbe1961d60
hlWang <whl413035073@qq.com>
2017-07-11 23:47:58 +0800

    debug

--- sen_pro.py
+++ sen_pro.py
@@ -12,4 +12,28 @@ def senten_tag(sentence):
 
 
 def diction_set(doc):
+    docs_word = []
+    docs_ne = []
+    for sen in doc:
+        for ite in sen:
+            docs_word.append(ite[0])
+            docs_ne.append(ite[-1])
+    word_set = set(docs_word)
+    ne_set = set(docs_ne)
+    word_dic = dict()
+    ne_dic = dict()
+    for word in word_set:
+        word_dic[word] = len(word_dic)
+    for ne in ne_set:
+        ne_dic[ne] = len(ne_dic)
+    pro_doc = []
+    for sen in doc:
+        pro_sen = []
+        for ite in sen:
+            pro_sen.append((word_dic[ite[0]] , ne_dic[ite[-1]]))
+        pro_doc.append(pro_sen)
+    word_reversed_dic = dict(zip(word_dic.values(), word_dic.keys()))
+    return pro_doc, word_dic, ne_dic, word_reversed_dic
+
+def style
 
