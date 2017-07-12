7d33751e08a19358acb51c1300d5c0b790319235
hlWang <whl413035073@qq.com>
2017-07-12 12:12:49 +0800

    add

--- sen_pro.py
+++ sen_pro.py
@@ -27,13 +27,65 @@ def diction_set(doc):
     for ne in ne_set:
         ne_dic[ne] = len(ne_dic)
     pro_doc = []
+    O_tag = u'O'
     for sen in doc:
         pro_sen = []
         for ite in sen:
             pro_sen.append((word_dic[ite[0]] , ne_dic[ite[-1]]))
         pro_doc.append(pro_sen)
     word_reversed_dic = dict(zip(word_dic.values(), word_dic.keys()))
-    return pro_doc, word_dic, ne_dic, word_reversed_dic
+    return pro_doc, word_dic, ne_dic, word_reversed_dic, ne_dic[O_tag]
 
-def style
+def style_extract(sen, O_value):
+    style = []
+    for i in range(len(sen)):
+        if sen[i][-1] == O_value:
+            style.append(sen[i][0])
+        else:
+            style.append(-1)
+    return style
+
+
+class sen_style:
+    def __init__(self, sen, style):
+        self.style = style
+        self.placenum = style.count(-1)
+        self.placedic = dict()
+        for i in range(len(sen)):
+            if style[i] == -1:
+                self.placedic[i] = [sen[i][-1]]
+        self.count = 1
+    
+    def style_eq(self, style):
+        if self.style == style:
+            return 1
+        else return 0
+
+    def style_add(self, sen):
+        self.count = self.count + 1
+        for i in range(len(sen)):
+            if self.style[i] == -1:
+                if sen[i][-1] not in self.placedic[i]:
+                    self.placedic[i].append(sen[i][-1])
+
+
+def build_style_list(doc, O_value):
+    style_list = []
+    for sen in doc:
+        style = style_extract(sen, O_value)
+        if style_list == []:
+            sensty = sen_style(sen, style)
+            style_list.append(sensty)
+        else:
+            style_flag = 0
+            for sen_sty in style_list:
+                if sen_sty.style_eq(style):
+                    style_flag = 1
+                    break
+            if style_flag == 0:
+                sensty = 
+
+
+
+            
 
